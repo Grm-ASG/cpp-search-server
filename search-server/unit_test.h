e19d2ebebe0093f515695e4acadc8a803b59e23f
Roman Guryanov <guryanovroman123@gmail.com>
2022-03-13 21:36:30 +0300

    8 sprint

--- search-server/unit_test.h
+++ search-server/unit_test.h
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <tuple>
 
+using std::string_literals::operator""s;
+
 #define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, ""s)
 #define ASSERT_EQUAL_HINT(a, b, hint) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, (hint))
 #define ASSERT(expr) AssertImpl(!!(expr), #expr, __FILE__, __FUNCTION__, __LINE__, ""s)
@@ -19,17 +21,16 @@
 void TestSearchServer();
 
 template <typename T, typename U>
-void AssertEqualImpl( const T& t, const U& u, const std::string& t_str, const std::string& u_str, const std::string& file,
-                      const std::string& func, unsigned line, const std::string& hint )
+void AssertEqualImpl(const T& t, const U& u, const std::string& t_str, const std::string& u_str, const std::string& file,
+    const std::string& func, unsigned line, const std::string& hint)
 {
-    using std::string_literals::operator""s;
-    if ( t != u )
+    if (t != u)
     {
         std::cerr << std::boolalpha;
         std::cerr << file << "("s << line << "s): "s << func << ": "s;
         std::cerr << "ASSERT_EQUAL("s << t_str << ", "s << u_str << "s) failed: "s;
         std::cerr << t << " != "s << u << "."s;
-        if ( !hint.empty() )
+        if (!hint.empty())
         {
             std::cerr << " Hint: "s << hint;
         }
@@ -38,13 +39,12 @@ void AssertEqualImpl( const T& t, const U& u, const std::string& t_str, const st
     }
 }
 
-void AssertImpl( bool value, const std::string& expr_str, const std::string& file,
-                 const std::string& func, unsigned line, const std::string& hint );
+void AssertImpl(bool value, const std::string& expr_str, const std::string& file,
+    const std::string& func, unsigned line, const std::string& hint);
 
 template <typename Function>
-void RunTestImpl( Function func, const std::string& funcName )
+void RunTestImpl(Function func, const std::string& funcName)
 {
-    using std::string_literals::operator""s;
     func();
     std::cerr << funcName << " OK"s << std::endl;
 }
