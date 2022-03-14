ac4a4c916abff5a101fd011bc848723ebb5d6435
Roman Guryanov <guryanovroman123@gmail.com>
2022-03-14 13:08:26 +0300

    MOD code style

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -15,7 +15,7 @@ public:
 
 	template <typename DocumentPredicate>
 	std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
-	std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus statusDoc);
+	std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 	std::vector<Document> AddFindRequest(const std::string& raw_query);
 	int GetNoResultRequests() const;
 
@@ -33,11 +33,11 @@ private:
 template <typename DocumentPredicate>
 std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate)
 {
-	std::vector<Document> searchResult = server_.FindTopDocuments(raw_query, document_predicate);
+	std::vector<Document> search_result = server_.FindTopDocuments(raw_query, document_predicate);
 	if (requests_.size() >= min_in_day_)
 	{
 		requests_.pop_front();
 	}
 	requests_.push_back({ raw_query, static_cast<int>(searchResult.size()) });
-	return searchResult;
+	return search_result;
 }
